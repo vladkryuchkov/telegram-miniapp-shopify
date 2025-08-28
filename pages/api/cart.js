5fd62572a9297db356fbf1e19b77898c594dae2a
vladkryuchkov <vlad.kryuchkov@gmail.com>
2025-08-28 20:40:34 +0300

    Update cart.js

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -2,6 +2,8 @@ import { storefront, QUERIES } from "../../lib/shopify";
 
 export default async function handler(req, res) {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   try {
     if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 
